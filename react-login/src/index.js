2e1107ebf8d62cc9cef041b234601b9a9374942e
Siripat Srisuwannaphat <47081187+iristap@users.noreply.github.com>
2024-01-20 00:10:15 +0700

    1

---
+++ react-login/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter , Routes, Route} from 'react-router-dom';
+import Login from './Login.js'
+import Album from './Album.js'
+import Register from './Register.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/album" element={<Album />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
